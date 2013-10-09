15f468f8a1099d58a828cda431d45c96639eb972
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:11:16 -0500

    in class

--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -4,7 +4,19 @@ Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Dim currentyear As Integer
+        Dim currentpopulation As Double
+
+        currentyear = 2012
+        currentpopulation = 7000
+
+        Do While (currentpopulation > 6)
+            currentyear = currentyear - 50
+            currentpopulation = currentpopulation / 2
+        Loop
+
+        MessageBox.Show("The year is " & currentyear, "Error")
 
     End Sub
+
 End Class
